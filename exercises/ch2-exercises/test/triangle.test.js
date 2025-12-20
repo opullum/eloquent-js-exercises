934ce1958b4d0a69b0318da507d9e4f02293d688
opullum <octavian.pullum@gmail.com>
2025-12-20 23:55:00 +0900

    Created test for problem Triangle from Chapter 2

--- exercises/ch2-exercises/test/triangle.test.js
+++ exercises/ch2-exercises/test/triangle.test.js
@@ -1,3 +1,17 @@
 import { solution } from '../solutions/triangle-solution.js';
 import { expect } from 'chai';
-import { sinon } from 'sinon';
+import sinon from 'sinon';
+
+const expectedResult = "#\n" + "##\n" + "###\n" + "####\n" + "#####\n" + "######\n" + "#######";
+
+describe('Solution testing for problem "Looping a Triangle" from Chapter 2', () => {
+    it('Function solution() matches the expected output given 7 console.log() calls', () => {
+        const consoleLogStub = sinon.stub(console, 'log');
+        solution();
+        const lines = consoleLogStub.getCalls().map((call) => {return call.args[0]});
+        consoleLogStub.restore();
+
+        const actual = lines.join('\n');
+        expect(actual).to.equal(expectedResult);
+    }) 
+});
