4a56b4b109fc079ac92a0205052a9ca697c5ade3
opullum <octavian.pullum@gmail.com>
2025-12-23 15:57:36 +0900

    Accidentally pushed the textbook problem solution while verifying unit tests. Removed.

--- exercises/ch5-exercises/solutions/writing-solution.js
+++ exercises/ch5-exercises/solutions/writing-solution.js
@@ -35,12 +35,5 @@ function characterScript(code) {
 
 
 export function dominantDirection(text) {
-  let counted = countBy(text, char => {
-    let script = characterScript(char.codePointAt(0));
-    return script ? script.direction : "none";
-  }).filter(({name}) => name != "none");
-
-  if (counted.length == 0) return "ltr";
-
-  return counted.reduce((a, b) => a.count > b.count ? a : b).name;
+    // Your code here.
 }
